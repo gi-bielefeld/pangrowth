d499a6b6ec75b8e6b7ada418a8ebd0c3c84d4843
lucaparmigiani <luca.parmigiani@studenti.univr.it>
2024-11-07 17:08:00 +0100

    added infix storage

--- src/yak-hist.h
+++ src/yak-hist.h
@@ -9,9 +9,10 @@
 #include "kthread.h"
 #include "ketopt.h"
 
-#define CALLOC(ptr, len) ((ptr) = (__typeof__(ptr))calloc((len), sizeof(*(ptr))))
-#define MALLOC(ptr, len) ((ptr) = (__typeof__(ptr))malloc((len) * sizeof(*(ptr))))
-#define REALLOC(ptr, len) ((ptr) = (__typeof__(ptr))realloc((ptr), (len) * sizeof(*(ptr))))
+#define PRINT_BITS(num) {int _ii; for(_ii = sizeof((__typeof__(num))num)*8-1; _ii>=0; _ii--) printf("%c", (num & (1ULL << _ii))?'1':'0'); printf("\n");}
+#define mcalloc(pos, len) ((pos) = (__typeof__(pos))calloc((len), sizeof(*(pos))))
+#define mmalloc(pos, len) ((pos) = (__typeof__(pos))malloc((len) * sizeof(*(pos))))
+#define mrealloc(pos, len) ((pos) = (__typeof__(pos))realloc((pos), (len) * sizeof(*(pos))))
 
 //#define MAX_KMER     31
 #define N_COUNTS     8192
@@ -20,7 +21,7 @@
 char* bits2kmer(uint64_t kmer_bits, int k) {
     char nucleotides[4] = {'A','C','G','T'};
     char *kmer_char;
-    CALLOC(kmer_char, k+1);
+    mcalloc(kmer_char, k+1);
     for (int i = 0; i < k; i++) {
         kmer_char[i] = nucleotides[(kmer_bits>>(2*k-(i+1)*2)) & 3];
     }
@@ -28,7 +29,7 @@ char* bits2kmer(uint64_t kmer_bits, int k) {
     return kmer_char;
 }
 
-int ID_GENOME = 1;
+uint32_t ID_GENOME = 1;
 int NUM_GENOMES;
 int TOTAL_BITS;
 int BITS_GENOME;
@@ -36,9 +37,16 @@ int MASK_COUNT;
 int MASK_GENOME;
 int SUF;
 
-#define ch_eq(a, b) ((a) == (b)) // lower 8 bits for counts; higher bits for k-mer
+typedef struct {
+    uint32_t edges[16];
+    uint8_t last_edge;
+    uint32_t last_genome;
+    uint32_t sigma; 
+} infix_storage_t;
+
+#define ch_eq(a, b) ((a) == (b))
 #define ch_hash(a) ((a))
-KHASHL_MAP_INIT(, hashtable_t, ht, uint64_t, int32_t, ch_hash, ch_eq)
+KHASHL_MAP_INIT(, hashtable_t, ht, uint64_t, infix_storage_t, ch_hash, ch_eq)
 
 typedef struct {
 	int32_t k;
@@ -59,9 +67,7 @@ typedef struct {
 	ch1_t *ht;
 } multi_hashtable_t;
 
-
-static inline uint64_t hash64(uint64_t key, uint64_t mask) // invertible integer hash function
-{
+static inline uint64_t hash64(uint64_t key, uint64_t mask) { // invertible integer hash function
 	//key = (~key + (key << 21)) & mask; // key = (key << 21) - key - 1;
 	//key = key ^ key >> 24;
 	//key = ((key + (key << 3)) + (key << 8)) & mask; // key * 265
@@ -72,14 +78,20 @@ static inline uint64_t hash64(uint64_t key, uint64_t mask) // invertible integer
 	return key;
 }
 
+static inline uint64_t get_infix (uint64_t kmer_bits, int k) {
+    uint64_t mask = (1 << (2 * (k - 1))) - 1;
+    return (kmer_bits >> 2) & mask;
+}
+
+
 /*** hash table ***/
 multi_hashtable_t *ch_init(int k, int suf) {
 	multi_hashtable_t *ht;
 	int i;
 	//if (suf < TOTAL_BITS) return 0; //suf must be greater or equal (here is equal)
-	CALLOC(ht, 1);
+	mcalloc(ht, 1);
 	ht->k = k, ht->suf = suf;
-	CALLOC(ht->ht, 1<<ht->suf);
+	mcalloc(ht->ht, 1<<ht->suf);
 	for (i = 0; i < 1<<ht->suf; ++i)
 		ht->ht[i].ht = ht_init();
 	return ht;
@@ -95,55 +107,79 @@ void ch_destroy(multi_hashtable_t *ht) {
 
 int ch_insert_list(multi_hashtable_t *ht, int n, const uint64_t *a) {
 	if (n == 0) return 0;
-    uint64_t mask = ((1<<(ht->suf+2))-4);
+    //uint64_t mask = ((1<<(ht->suf+2))-4);
+    uint64_t mask = (1<<(ht->suf))-1;
     int n_ins = 0;
-	ch1_t *g = &ht->ht[(a[0]&mask)>>2];
+    int k = ht->k;
+    //hashtable_t *h = ht->ht[(a[0]&mask)>>2].ht;
+    hashtable_t *h = ht->ht[(get_infix(a[0],k)&mask)].ht;
 	for (int j = 0; j < n; ++j) {
+        uint64_t kp1mer = a[j];
+        uint64_t infix = get_infix(kp1mer, k);
+        uint8_t first_nt = (kp1mer >> (2 * k));
+        uint8_t last_nt = kp1mer & 3ULL;
+        uint8_t combined_nt = ((first_nt << 2) | last_nt);
+        //printf("%s\n", bits2kmer(kp1mer,k+1)); 
+        //printf("%s\n\n", bits2kmer(infix, k+1)); 
 		int absent;
-        //khint_t k = ht_put(g->ht, a[j]&(~mask), &absent);
-        khint_t k = ht_put(g->ht, a[j], &absent);
-        //printf("%s %d\n",bits2kmer(a[j],ht->k+1), absent);
-        if (absent) ++n_ins;
-        if (((kh_val(g->ht, k)&MASK_GENOME) >> BITS_GENOME) != ID_GENOME) {
-            kh_val(g->ht, k) = ((kh_val(g->ht, k)+1) & (~MASK_GENOME)) | (ID_GENOME << BITS_GENOME);
+        khint_t z = ht_put(h, infix, &absent);
+        //printf("%s %d\n", bits2kmer(infix,k-1), absent);
+        if (absent) { 
+            n_ins++;
+            memset(kh_val(h,z).edges, 0, 16*sizeof(uint32_t));
+            kh_val(h,z).last_edge = combined_nt;
+            kh_val(h,z).edges[kh_val(h,z).last_edge]++;
+            kh_val(h,z).last_genome = ID_GENOME;
+            kh_val(h,z).sigma = 1;
+        } else if (kh_val(h,z).last_genome == ID_GENOME && 
+                   kh_val(h,z).last_edge != combined_nt && 
+                   kh_val(h,z).last_edge != 255) {
+            kh_val(h,z).edges[kh_val(h,z).last_edge]--;
+            kh_val(h,z).last_edge = 255;
+        } else if (kh_val(h,z).last_genome != ID_GENOME) {
+            kh_val(h,z).last_edge = combined_nt;
+            kh_val(h,z).edges[kh_val(h,z).last_edge]++;
+            kh_val(h,z).last_genome = ID_GENOME;
+            kh_val(h,z).sigma++;
         }
+        //printf("%p %s(%d) %d %d\n", &kh_val(h,z), bits2kmer(kh_val(h,z).last_edge, 2), kh_val(h,z).last_edge, kh_val(h,z).last_genome, kh_val(h,z).sigma);
 	}
 	return n_ins;
 }
 
 /*** generate histogram ***/
-typedef struct {
-	uint64_t c[N_COUNTS];
-} buf_cnt_t;
+//typedef struct {
+//	uint64_t c[N_COUNTS];
+//} buf_cnt_t;
+//
+//typedef struct {
+//	const multi_hashtable_t *ht;
+//	buf_cnt_t *cnt;
+//} hist_aux_t;
 
-typedef struct {
-	const multi_hashtable_t *ht;
-	buf_cnt_t *cnt;
-} hist_aux_t;
-
-static void worker_hist(void *data, long i, int tid) { // callback for kt_for()
-	hist_aux_t *a = (hist_aux_t*)data;
-	uint64_t *cnt = a->cnt[tid].c;
-	hashtable_t *g = a->ht->ht[i].ht;
-	khint_t j;
-	for (j = 0; j < kh_end(g); ++j)
-		if (kh_exist(g, j))
-            ++cnt[kh_val(g, j)&MASK_COUNT];
-}
-
-void hist(const multi_hashtable_t *ht, int64_t cnt[N_COUNTS], int n_thread) {
-	hist_aux_t a;
-	int i, j;
-	a.ht = ht;
-	memset(cnt, 0, (NUM_GENOMES+1) * sizeof(uint64_t));
-	CALLOC(a.cnt, n_thread); // count is divide for number of threads
-	kt_for(n_thread, worker_hist, &a, 1<<ht->suf);
-	for (i = 0; i <= NUM_GENOMES; ++i) cnt[i] = 0; //This will be the total amount
-	for (j = 0; j < n_thread; ++j)
-		for (i = 0; i <= NUM_GENOMES; ++i)
-			cnt[i] += a.cnt[j].c[i]; //Combine all
-	free(a.cnt);
-}
+//static void worker_hist(void *data, long i, int tid) { // callback for kt_for()
+//	hist_aux_t *a = (hist_aux_t*)data;
+//	uint64_t *cnt = a->cnt[tid].c;
+//	hashtable_t *g = a->ht->ht[i].ht;
+//	khint_t j;
+//	//for (j = 0; j < kh_end(g); ++j)
+//	//	if (kh_exist(g, j))
+//    //        ++cnt[kh_val(g, j)&MASK_COUNT];
+//}
+//
+//void hist(const multi_hashtable_t *ht, int64_t cnt[N_COUNTS], int n_thread) {
+//	hist_aux_t a;
+//	int i, j;
+//	a.ht = ht;
+//	memset(cnt, 0, (NUM_GENOMES+1) * sizeof(uint64_t));
+//	mcalloc(a.cnt, n_thread); // count is divide for number of threads
+//	kt_for(n_thread, worker_hist, &a, 1<<ht->suf);
+//	for (i = 0; i <= NUM_GENOMES; ++i) cnt[i] = 0; //This will be the total amount
+//	for (j = 0; j < n_thread; ++j)
+//		for (i = 0; i <= NUM_GENOMES; ++i)
+//			cnt[i] += a.cnt[j].c[i]; //Combine all
+//	free(a.cnt);
+//}
 
 
 void chomp(char *str) {
@@ -200,32 +236,38 @@ typedef struct {
 } ch_buf_t;
 
 // insert a k-mer $y to a linear buffer
-static inline void ch_insert_buf(ch_buf_t *buf, int suf, uint64_t y) {
-    uint64_t mask = ((1<<(suf+2))-4);
-    uint64_t y_suf = (y & mask)>>2;
-    //printf("%s\n", bits2kmer(y, 5));
-    //printf("%s %ld\n\n", bits2kmer(y_suf, 5), y_suf);
+static inline void ch_insert_buf(ch_buf_t *buf, int suf, uint64_t y, int k) {
+    //uint64_t mask = ((1<<(suf+2))-4);
+    //uint64_t y_suf = (y & mask);
     //uint64_t y_suf = y & ((1<<suf) - 1);
+    uint64_t infix = get_infix(y, k);
+    uint64_t mask = (1<<(suf))-1;
+    uint64_t y_suf = (infix & mask);
+    //printf("%s\n",   bits2kmer(y, k+1));
+    //printf("%s\n",   bits2kmer(infix, k+1));
+    //printf("%s\n\n", bits2kmer(y_suf, k+1));
 	ch_buf_t *b = &buf[y_suf];
 	if (b->n == b->m) {
 		b->m = b->m < 8 ? 8 : b->m + (b->m>>1);
-		REALLOC(b->a, b->m);
+		mrealloc(b->a, b->m);
 	}
 	b->a[b->n++] = y;
 }
 
 // insert canonical k-mers in $seq to linear buffer $buf
+// k = k+1 here 
 static void count_seq_buf_can(ch_buf_t *buf, int k, int suf, int len, const char *seq){ 
 	int i, l;
-	uint64_t x[2], mask = (1ULL<<k*2) - 1, shift = (k - 1) * 2;
+	uint64_t x[2], mask = (1ULL<<(k+1)*2) - 1, shift = (k+1 - 1) * 2;
 	for (i = l = 0, x[0] = x[1] = 0; i < len; ++i) {
 		int c = seq_nt4_table[(uint8_t)seq[i]];
 		if (c < 4) { // not an "N" base
 			x[0] = (x[0] << 2 | c) & mask;                  // forward strand
 			x[1] = x[1] >> 2 | (uint64_t)(3 - c) << shift;  // reverse strand
-			if (++l >= k) { // we find a k-mer
-				uint64_t y = x[0] < x[1]? x[0] : x[1];      // Canonicals!
-				ch_insert_buf(buf, suf, hash64(y, mask));
+			if (++l >= k+1) { // we found a k+1-mer
+                uint64_t infix[2] = {get_infix(x[0], k), get_infix(x[1], k)};
+				uint64_t y = infix[0] < infix[1] ? x[0] : x[1];      // Canonicals!
+				ch_insert_buf(buf, suf, hash64(y, mask), k);
 			}
 		} else l = 0, x[0] = x[1] = 0; // if there is an "N", restart
 	}
@@ -234,13 +276,13 @@ static void count_seq_buf_can(ch_buf_t *buf, int k, int suf, int len, const char
 // insert k-mers in $seq to linear buffer $buf
 static void count_seq_buf(ch_buf_t *buf, int k, int suf, int len, const char *seq){ 
 	int i, l;
-	uint64_t x, mask = (1ULL<<k*2) - 1;
+	uint64_t x, mask = (1ULL<<(k+1)*2) - 1;
 	for (i = l = 0, x = 0; i < len; ++i) {
 		int c = seq_nt4_table[(uint8_t)seq[i]];
 		if (c < 4) { // not an "N" base
 			x = (x << 2 | c) & mask;                  // forward strand
-			if (++l >= k) { // we find a k-mer
-				ch_insert_buf(buf, suf, hash64(x, mask));
+			if (++l >= k+1) { // we found a k+1-mer
+				ch_insert_buf(buf, suf, hash64(x, mask), k);
 			}
 		} else l = 0, x = 0; // if there is an "N", restart
 	}
@@ -272,17 +314,17 @@ static void *worker_pipeline(void *data, int step, void *in) { // callback for k
 	if (step == 0) { // step 1: read a block of sequences
 		int ret;
 		stepdat_t *s;
-		CALLOC(s, 1);
+		mcalloc(s, 1);
 		s->p = p;
 		while ((ret = kseq_read(p->kseq)) >= 0) {
 			int l = p->kseq->seq.l;
 			if (l < p->param->k) continue;
 			if (s->n == s->m) {
 				s->m = s->m < 16? 16 : s->m + (s->n>>1);
-				REALLOC(s->len, s->m);
-				REALLOC(s->seq, s->m);
+				mrealloc(s->len, s->m);
+				mrealloc(s->seq, s->m);
 			}
-			MALLOC(s->seq[s->n], l);
+			mmalloc(s->seq[s->n], l);
 			memcpy(s->seq[s->n], p->kseq->seq.s, l);
 			s->len[s->n++] = l;
 			s->sum_len += l;
@@ -296,20 +338,20 @@ static void *worker_pipeline(void *data, int step, void *in) { // callback for k
 		stepdat_t *s = (stepdat_t*)in;
 		int i, m;
         int n = 1<<p->param->suf;
-		CALLOC(s->buf, n);
+		mcalloc(s->buf, n);
 		m = (int)(s->nk * 1.2 / n) + 1;
 		for (i = 0; i < n; ++i) {
 			s->buf[i].m = m;
-			MALLOC(s->buf[i].a, m);
+			mmalloc(s->buf[i].a, m);
 		}
         if (p->param->canonical) {
             for (i = 0; i < s->n; ++i) {
-                count_seq_buf_can(s->buf, p->param->k+1, p->param->suf, s->len[i], s->seq[i]);
+                count_seq_buf_can(s->buf, p->param->k, p->param->suf, s->len[i], s->seq[i]);
                 free(s->seq[i]);
             }
         } else {
             for (i = 0; i < s->n; ++i) {
-                count_seq_buf(s->buf, p->param->k+1, p->param->suf, s->len[i], s->seq[i]);
+                count_seq_buf(s->buf, p->param->k, p->param->suf, s->len[i], s->seq[i]);
                 free(s->seq[i]);
             }
         }
@@ -370,17 +412,31 @@ uint32_t count_fasta(char* filelist) {
     return num_lines;
 }
 
-void print_kmer_debug(multi_hashtable_t* ht) {
-    int i;
-    uint32_t j;
-	for (i = 0; i < 1<<ht->suf; ++i) {
-	    hashtable_t *g = ht->ht[i].ht;
-        for (j = 0; j < kh_end(g); ++j)
-            if (kh_exist(g, j))
-                printf("[%d] %s\n", i, bits2kmer(kh_key(g, j), ht->k+1));
+void print_infix_debug(multi_hashtable_t* ht) {
+    for (int i = 0; i < ht->k; i++) {
+        printf(" ");
+    }
+    printf("\t");
+    char nucleotides[4] = {'A','C','G','T'};
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%c%c ",nucleotides[i], nucleotides[j]);
+        }
+    }
+    printf(" sig\tlastG\tlastE\n");
+	for (int i = 0; i < 1<<ht->suf; ++i) {
+	    hashtable_t *h = ht->ht[i].ht;
+        for (uint32_t z = 0; z < kh_end(h); ++z)
+            if (kh_exist(h,z)) {
+                printf("%s\t", bits2kmer(kh_key(h,z), ht->k-1));
+                for (int j = 0; j < 16; j++) {
+                    printf("%d  ", kh_val(h,z).edges[j]);
+                }
+                printf(" %d\t%d\t%s(%d)", kh_val(h,z).sigma, kh_val(h,z).last_genome, bits2kmer(kh_val(h,z).last_edge, 2), kh_val(h,z).last_edge);
+                printf("\n");
+            }
     }
 }
-
 
 void output_hist(int argc, char *argv[]){
 	multi_hashtable_t *ht = 0;
@@ -463,12 +519,11 @@ void output_hist(int argc, char *argv[]){
     }
 	fprintf(stderr, "[M::%s] %ld distinct k-mers\n", __func__, (long)ht->tot);
 
-	int64_t cnt[N_COUNTS];
+	print_infix_debug(ht);
 
-	//print_kmer_debug(ht);
-
-	hist(ht, cnt, param.n_thread);
+	//int64_t cnt[N_COUNTS];
+	//hist(ht, cnt, param.n_thread);
 	//for (i = 1; i <= NUM_GENOMES; ++i) printf("%d\t%lld\n", i,(long long)cnt[i]);
-	for (i = 1; i <= NUM_GENOMES; ++i) printf("%lld\n", (long long)cnt[i]);
+	//for (i = 1; i <= NUM_GENOMES; ++i) printf("%lld\n", (long long)cnt[i]);
 	ch_destroy(ht);
 }
